4c33b25a580f91225cf4a643051a57e0bf9396d8
Kasandra Orta <kasandraorta26@gmail.com>
2023-06-07 17:11:59 -0600

    Initial commit

---
+++ plants.js
@@ -0,0 +1,8 @@
+var gets_sunshine = true
+var ounces_of_water = 15
+
+If (gets_sunshine === true && ounces_of_water >= 10) {
+    console.log("This is a happy plant.");
+} else {
+    console.log("Maybe I shouldn't have plants.");
+}
