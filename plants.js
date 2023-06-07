eb35a0dc642aeec6b46757ccfd2c65f57192e52d
Kasandra Orta <kasandraorta26@gmail.com>
2023-06-07 17:18:37 -0600

    Change variable values

--- plants.js
+++ plants.js
@@ -1,7 +1,8 @@
 var gets_sunshine = true
-var ounces_of_water = 15
+var ounces_of_water = 9
 
 If (gets_sunshine === true && ounces_of_water >= 10) {
+
     console.log("This is a happy plant.");
 } else {
     console.log("Maybe I shouldn't have plants.");
